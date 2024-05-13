7c831cd41b1fa9a58695e8381f715e21c818a2a2
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-05-13 18:04:55 +0700

    Chore: Clean unused program

--- handler/auth.go
+++ handler/auth.go
@@ -4,15 +4,9 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type Row struct {
-	Id       int
-	Username string
-	Email     string
-}
-
 func Login(ctx *fiber.Ctx) error {
 	return ctx.JSON(fiber.Map{
-		"status": "success",
+		"status":  "success",
 		"message": "Login successfully!",
 	})
 }
